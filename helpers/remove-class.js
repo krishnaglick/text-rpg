0a9a5ff688999b235b3e3deae90a61b89fd5debf
Krishna Glick <unstoppable411@gmail.com>
2017-08-31 20:25:03 -0400

    testing is important, I should do it

--- helpers/remove-class.js
+++ helpers/remove-class.js
@@ -6,10 +6,10 @@ const ask = require('./ask-question');
 const loadClasses = require('./load-classes');
 
 const deleteConfirmation = async ({ name, file }, goBack) => {
+  console.log(name, file);
   const answer = await ask(`Are you sure you want to remove ${name}? Y/N`);
   if(['Y', 'y', 'yes'].includes(answer)) {
     fs.unlinkSync(file);
-    console.log('Deleted');
     goBack();
   }
   else if(['N', 'n', 'no'].includes(answer))
@@ -19,8 +19,12 @@ const deleteConfirmation = async ({ name, file }, goBack) => {
 };
 
 module.exports = async (goBack) => {
-  const classes = loadClasses(goBack);
-  Object.keys(classes).forEach(k => classes[k] = deleteConfirmation({ name: k, file: classes[k]}, goBack));
+  const classes = loadClasses();
+  Object.keys(classes).forEach(k => {
+    const name = k;
+    const file = classes[k];
+    classes[k] = async () => await deleteConfirmation({ name, file }, goBack);
+  });
   classes['Go Back'] = goBack;
   const { choice }  = await inquirer.prompt({
     type: 'list',
@@ -28,5 +32,11 @@ module.exports = async (goBack) => {
     name: 'choice',
     choices: Object.keys(classes)
   });
-  await classes[choice](goBack);
+  try {
+    await classes[choice](goBack);
+  }
+  catch(x) {
+    console.error(x);
+    module.exports(goBack);
+  }
 };
